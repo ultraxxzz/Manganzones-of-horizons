96655effce745a7f2c9467ea254a4178b75810ed
ultraxxzz <josemanuelalbaescalante@gmail.com>
2024-09-29 23:39:06 +0000

    Mundo guardado 2024-09-29

--- server.py
+++ server.py
@@ -6,7 +6,7 @@
 # Puedes instalar purpur después de instalar fabric desde el menú de gestionar
 
 # Regiones de ngrok
-# Código          Lugar
+# Código          lugar
 #-----------      ---------------------------
 # ap	          Asia / Pacífico (Singapore)
 # au		      Australia (Sydney)
